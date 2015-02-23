459ae00d9d0e310a51bc4e41c1157a7c950fbd19
Chris DiMartino <chris.dimartino@gmail.com>
2015-02-23 16:59:01 -0500

    Adding environment.rb and database.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 get '/' do
   # Inspects all of the methods of the request objects
-  puts request.inspect
+  # puts request.inspect
 
   # Return with a response code: 200 and body
   'Welcome To Phase 2 Fiddler Crabs - Crabuluous!'
