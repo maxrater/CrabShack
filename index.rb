c65a36e0a3339ddb0f4973b3a16c5c521f150c2c
Zack Mance <zmance@gmail.com>
2015-02-22 23:14:07 -0500

    Adds Comments for clarity

--- index.rb
+++ index.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  puts "Zack"
+  #Inspects all of the methods of the request objects 
   puts request.inspect
+
+  #Return with a response code: 200 and body 
   " Welcome To Phase 2 Fiddler Crabs "
+
 end
 
