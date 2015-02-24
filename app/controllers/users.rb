370fe2c009749b048dd64b703934aeb29403415f
Zack Mance <zmance@gmail.com>
2015-02-24 08:50:01 -0500

    Adds Basic REST / CRUD routes for user

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -3,3 +3,27 @@ SAVED_USERS = [{id: 1, user:  User.new(name: 'zack', password:'1234') },
                {id: 3, user:  User.new(name: 'jon', password:'1234') },
                {id: 4, user:  User.new(name: 'brendon', password:'1234') }]
 
+#Index / Return All Users
+get '/users' do
+
+end
+
+#Show / Return the user with :id
+get '/users/:id' do
+
+end
+
+#Create / Adds new user
+post '/users/create' do
+
+end
+
+#Update / Updates user with ID
+put '/users/:id' do
+
+end
+
+#Destroy / Removes the users from the system 
+delete '/users/:id' do
+
+end
